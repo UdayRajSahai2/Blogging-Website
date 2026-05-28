1446bb716df65063311b710f94e6aad437ffbcda
Prabhat Ranjan <prabhat.ranjan@zohomail.in>
2026-05-28 11:48:49 +0530

    added table name student_enrollment

--- server/models/student/StudentEnrollment.js
+++ server/models/student/StudentEnrollment.js
@@ -1,32 +1,39 @@
-import { DataTypes, Op } from "sequelize";
+import { DataTypes } from "sequelize";
 import sequelize from "../../config/db.config.js";
-const StudentEnrollment = sequelize.define("StudentEnrollment", {
-  enrollment_id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
 
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
+const StudentEnrollment = sequelize.define(
+  "StudentEnrollment",
+  {
+    enrollment_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
 
-  referrer_first_name: {
-    type: DataTypes.STRING(50),
-  },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
 
-  referrer_last_name: {
-    type: DataTypes.STRING(50),
-  },
+    referrer_first_name: {
+      type: DataTypes.STRING(50),
+    },
 
-  referrer_mobile: {
-    type: DataTypes.STRING(10),
-  },
+    referrer_last_name: {
+      type: DataTypes.STRING(50),
+    },
 
-  referrer_district: {
-    type: DataTypes.STRING(100),
+    referrer_mobile: {
+      type: DataTypes.STRING(10),
+    },
+
+    referrer_district: {
+      type: DataTypes.STRING(100),
+    },
   },
-});
+  {
+    tableName: "student_enrollments", // IMPORTANT
+  },
+);
 
 export default StudentEnrollment;
